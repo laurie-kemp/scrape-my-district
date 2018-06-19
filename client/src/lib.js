61391b99d44472d22e84ac5233704c028d363501
kozlowskam <malgorzatta.kozlowska@gmail.com>
2018-06-19 16:46:51 +0200

    hands on table

--- client/src/lib.js
+++ client/src/lib.js
@@ -2,22 +2,16 @@ export const rows = [
   {
     id: 1,
     company_name: "22tracks",
-    website: "22tracks.com",
-    email: "info@22tracks.com",
-    city: "Amsterdam"
+    website: "22tracks.com"
   },
   {
     id: 2,
     company_name: "44tips",
-    website: "www.44tips.com",
-    email: "eric@44tips.com",
-    city: "The Hague"
+    website: "www.44tips.com"
   },
   {
     id: 3,
     company_name: "absrd.com",
-    website: "absrd.com",
-    email: "info@absrd.com",
-    city: "Utrecht"
+    website: "absrd.com"
   }
 ];
