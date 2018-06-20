1df424d32bf9f3422e11fae77e0d57ac7f1b31f3
fong1980 <fong1980@gmail.com>
2018-06-20 14:27:00 +0200

    working on login

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import LoginPage from "./components/login/LoginPage";
+// /Users/fong/code/scrape-rwapp/scrape-my-district/client/src/components
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+          LoginForm
+        </div>
+      </Router>
     );
   }
 }
