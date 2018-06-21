54d69cdde51126abf9fa6fc481454265f25e9c82
fong1980 <fong1980@gmail.com>
2018-06-21 11:22:16 +0200

    add material ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import LoginPage from "./components/login/LoginPage";
+import TopBar from "./components/layout/TopBar";
+
 // /Users/fong/code/scrape-rwapp/scrape-my-district/client/src/components
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <nav>
+            <TopBar />
+            test
+          </nav>
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/" render={() => <Redirect to="/login" />} />
-          Laurie" lastName="Kemp" email="laurie.kemp@scaleupnation.com"
-          password="Laurie999"
+          LoginForm
         </div>
       </Router>
     );
