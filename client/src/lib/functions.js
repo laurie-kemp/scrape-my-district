b2dfb00f909729d98775eefdfc393d88fc2de76e
Unknown <nokid_gr@hotmail.it>
2018-06-25 11:41:53 +0200

    fixes

--- client/src/lib/functions.js
+++ client/src/lib/functions.js
@@ -1,22 +1,79 @@
 export const changes = (databases, csv) => {
   let changes = [];
   let change = [];
+  let comp = [];
   if (databases) {
-    change = Object.keys(csv).forEach(company => {
-      //   console.log(databases[company].venture, "COMPANY NAME");
-      change = Object.keys(databases).foEach(entry => {
-        if(databases[entry].HQ_source !== csv[company].Location) console.log(csv[company].Location) 
-        else null;
-        // console.log(databases[entry], "ENTRY");
-        
-      });
-      console.log(change, "CHANGE");
+    Object.keys(csv).map(company => {
+      // console.log(csv[company].Name, "COMPANY NAME");
+      comp = Object.keys(databases)
+        .map(entry => {
+          if (databases[entry].venture === csv[company].Name) {
+            let location;
+            if (databases[entry].HQ_source !== csv[company].Location) {
+              location = csv[company].Location;
+            } else {
+              location = "same";
+            }
+            const correctAddress = csv[company].Location.includes(
+              "The Netherlands"
+            );
+            let newAddress = "";
+            if (correctAddress === false) {
+              console.log(newAddress, "WRONG ADDRESS FOUND");
+              return {
+                id: databases[entry].id,
+                venture: databases[entry].venture,
+                csv_location: csv[company].Location,
+                HQ_source: databases[entry].HQ_source,
+                LOCATION: newAddress
+              };
+            } else {
+              return {
+                id: databases[entry].id,
+                venture: databases[entry].venture,
+                csv_location: csv[company].Location,
+                HQ_source: databases[entry].HQ_source,
+                LOCATION: location
+              };
+            }
+          } else return false;
+          // console.log(databases[entry].HQ_source, "HQ SOURCE DATABASES");
+          // console.log(csv[company].Location, "CSV LOCATION");
+          // if (
+          //   databases[entry] &&
+          //   databases[entry].HQ_source &&
+          //   databases[entry].HQ_source === csv[company].Location
+          // ) {
+          //   return csv[company].Location;
+          // } else return false;
+          // return company;
+          // console.log(databases[entry], "ENTRY");
+        })
+        .filter(x => {
+          if (x !== true) return x;
+        });
+      if (comp) console.log(comp, "OBJECT");
+      if (comp.length > 1) {
+        comp.map(entry =>
+          console.log(
+            "multiple entries for " + entry.venture + " with ID: " + entry.id
+          )
+        );
+        // console.log(
+        //   "multiple entries for " +
+        //     comp[0].venture +
+        //     " please check your data manually. Ids are: "
+        // );
+      }
+      changes.push(comp);
+ 
       //console.log(csv[company].Location, "CITY");
     });
-    changes.push(change)
-    console.log(changes, "CHANGES");
-    return changes
-    
+    // console.log(changes, "CHANGES");
   }
+  const newChanges = changes.filter(entry => {
+    entry.HQ_source !== entry.csv_location;
+  });
+  console.log(newChanges, "CHANGES");
   // return changes
-}
+ };
