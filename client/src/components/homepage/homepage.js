2d89586f717b88bcbab3d2a8e16cb1a35714406c
fong1980 <fong1980@gmail.com>
2018-06-26 14:50:17 +0200

    add upload

--- client/src/components/homepage/homepage.js
+++ client/src/components/homepage/homepage.js
@@ -1,22 +1,18 @@
 import React, { PureComponent } from "react";
 import FileSelector from "../FileSelector";
 import Card from "@material-ui/core/Card";
-
+import Button from "@material-ui/core/Button";
 
 export default class Homepage extends PureComponent {
   render() {
     return (
       <div>
-        {/* <Card className={classes.card}> */}
         <Card>
-          <card>test</card>
           <div>
             <FileSelector />
           </div>
           <div>upload</div>
-
           <div>3r button</div>
-          <div>4e button</div>
         </Card>
       </div>
     );
