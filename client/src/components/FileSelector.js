c646a8b53fee0a8b2c4b2f708c0b116f51771c16
fong1980 <fong1980@gmail.com>
2018-06-27 12:07:30 +0200

    button done

--- client/src/components/FileSelector.js
+++ client/src/components/FileSelector.js
@@ -36,7 +36,6 @@ export class FileSelector extends Component {
     console.log(this.state.data);
     return (
       <div>
-        {/* <input type="file" onChange={e => this.handleChange(e.target.files)} /> */}
         <input
           style={{ display: "none" }}
           id="contained-button-file"
@@ -45,12 +44,7 @@ export class FileSelector extends Component {
         />
         <label htmlFor="contained-button-file">
           {this.state.filename}
-          <Button
-            // variant="contained"
-            component="div"
-            // color="primary"
-            className="upload-button"
-          >
+          <Button component="div" className="upload-button">
             Upload
             <FileUpload />
           </Button>
